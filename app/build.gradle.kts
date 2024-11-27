414be6e97528eb7a8bb138d227a601c276e89892
RehanGith <chrehan1007@gmail.com>
2024-11-27 08:03:26 +0500

    Created logic for box

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.colorsbyviews"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.colorsbyviews"
